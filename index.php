5d009e9a19164750d23c3d0dc3087dab7eb9a657
Ataulfo Paixao <ataulfo-92@hotmail.com>
2021-12-27 10:40:06 -0300

    inclusão de imagens nos campos de menus

--- index.php
+++ index.php
@@ -20,26 +20,85 @@
 <br>
 <div class="d-flex flex-column flex-shrink-0 p-3 text-white bg-dark" style="width: 210px; margin-top:-80px">
   <a href="index.php" class="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-white text-decoration-none">
-    <svg class="bi me-2" width="20" height="32"><use xlink:href="#bootstrap"/></svg>
+    <svg class="bi me-2" width="32" height="32">
+    
+<path style="fill:#000000; stroke:none;" d="M0 0L0 32L32 32L32 0L0 0z"/>
+<path style="fill:#4294ff; stroke:none;" d="M1 1L1 15L15 15L15 1L1 1M17 1L17 15L31 15L31 1L17 1M1 17L1 31L15 31L15 17L1 17M17 17L17 31L31 31L31 17L17 17z"/>
+
+
+    </svg>
     <span class="fs-4">PAINEL</span>
   </a>
   <hr>
   <ul class=" nav nav-pills flex-column mb-auto">
     <li class="selecionar dropdown-item">
       <a href="cadastro.html" class="nav-link text-white" aria-current="page">
-        <svg class="bi me-2" width="16" height="16"><use xlink:href="cadastro.html"/></svg>
+        <svg class="bi me-2" width="32" height="32">
+
+
+        <g transform="translate(0.000000,32.000000) scale(0.100000,-0.100000)"
+fill="#000000" stroke="none">
+<path d="M86 305 c-8 -8 -19 -15 -25 -15 -13 0 -14 -20 -2 -38 5 -9 24 -12 57
+-10 57 4 70 26 32 58 -28 24 -42 25 -62 5z"/>
+<path d="M12 268 c-16 -16 -16 -200 0 -216 7 -7 34 -12 61 -12 40 0 54 5 73
+25 l23 25 -59 0 c-32 0 -62 5 -65 10 -4 6 17 10 54 10 48 0 61 3 61 15 0 12
+-13 15 -61 15 -37 0 -58 4 -54 10 3 6 35 10 71 10 36 0 64 4 64 10 0 6 -30 10
+-70 10 -40 0 -70 4 -70 10 0 14 137 13 142 -1 6 -19 48 13 48 36 0 21 -25 65
+-32 55 -2 -3 -8 -14 -14 -25 -9 -16 -23 -21 -72 -23 -58 -3 -62 -2 -68 23 -7
+28 -14 31 -32 13z"/>
+<path d="M198 144 c-9 -20 -9 -30 0 -45 17 -27 15 -37 -10 -41 -17 -2 -24 -11
+-26 -30 l-3 -28 81 0 81 0 -3 28 c-2 19 -9 28 -25 30 -26 4 -28 14 -11 41 9
+15 9 25 0 45 -16 37 -68 37 -84 0z"/>
+</g>
+
+
+
+        </svg>
         Cadastro
       </a>
     </li>
     <li class="selecionar dropdown-item">
       <a href="lista_compradores.html" class="nav-link text-white">
-        <svg class="bi me-2" width="16" height="16"><use xlink:href="#speedometer2"/></svg>
+        <svg class="bi me-2" width="32" height="32">
+
+        <g transform="translate(0.000000,32.000000) scale(0.100000,-0.100000)"
+fill="#000000" stroke="none">
+<path d="M122 278 c-5 -7 -13 -27 -16 -44 -5 -24 -3 -33 9 -38 13 -4 15 -13 10 -36 -5 -23 -3 -30 8 -30 9 0 18 -6 21 -12 5 -10 7 -10 12 0 3 6 12 12 21 12 11 0 13 7 8 30 -5 23 -3 32 10 36 12 5 14 14 9 38 -9 43 -22 56 -54 56 -15 0 -32 -6 -38 -12z m57 -57 c21 -14 5 -43 -21 -39 -29 4 -39 48 -11 48 10 0 24
+-4 32 -9z m5 -60 c3 -5 -1 -14 -9 -21 -12 -10 -18 -10 -30 0 -18 15 -11 30 15
+30 10 0 21 -4 24 -9z"/>
+<path d="M71 126 c-7 -8 -13 -34 -14 -57 -2 -34 2 -44 15 -47 12 -2 18 5 21
+30 l4 33 1 -33 c2 -26 6 -33 20 -30 26 5 30 85 5 104 -23 17 -38 17 -52 0z"/>
+<path d="M197 126 c-24 -19 -21 -99 5 -104 12 -2 18 5 21 30 l4 33 1 -33 c2
+-26 6 -33 20 -30 13 3 17 13 15 47 -3 65 -28 86 -66 57z"/>
+<path d="M150 60 c0 -22 5 -40 10 -40 6 0 10 18 10 40 0 22 -4 40 -10 40 -5 0
+-10 -18 -10 -40z"/>
+</g>
+
+        </svg>
         Compradores
       </a>
     </li>
     <li class="selecionar dropdown-item">
       <a href="lista_fornecedores.html" class="nav-link text-white">
-        <svg class="bi me-2" width="16" height="16"><use xlink:href="#table"/></svg>
+        <svg class="bi me-2" width="32" height="32">
+
+
+        <g transform="translate(0.000000,32.000000) scale(0.100000,-0.100000)"
+fill="#000000" stroke="none">
+<path d="M62 308 c-19 -19 -14 -37 13 -44 32 -8 32 -24 0 -54 l-25 -24 37 -38
+c21 -22 47 -38 59 -38 13 0 29 -9 37 -20 13 -18 16 -16 72 40 58 58 58 60 40
+80 -23 25 -41 26 -48 0 -7 -26 -33 -36 -53 -19 -21 17 -14 51 13 65 21 10 21
+12 7 32 -19 27 -36 28 -59 2 -24 -27 -44 -25 -55 5 -11 28 -20 31 -38 13z"/>
+<path d="M0 65 c0 -58 2 -65 20 -65 18 0 20 7 20 65 0 58 -2 65 -20 65 -18 0
+-20 -7 -20 -65z"/>
+<path d="M50 66 l0 -56 80 0 c75 0 83 2 137 37 64 41 58 52 -14 30 -57 -18
+-145 -28 -137 -16 3 5 19 9 35 9 44 0 31 24 -17 32 -21 3 -49 9 -61 13 -22 7
+-23 5 -23 -49z"/>
+</g>
+
+
+
+        </svg>
         Fornecedores
       </a>
     </li>
--- js/arquivo.js
+++ js/arquivo.js
@@ -1,7 +1,9 @@
 
-document.getElementById('meuid').style.cursor = "pointer";
-document.getElementById('meuid').onclick = function(){
-window.location = 'cadastro.html'
 
+document.getElementById('estado_troca').onclick = function(){
+
+var recebe = document.getElementById('estado_troca').getAttribute('value');
+
+    console.log(recebe)
 
 }
