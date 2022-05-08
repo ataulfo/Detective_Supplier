e7891cde0f53a9e9a6cdc7a502bf58855534e559
Ataulfo Paixao <ataulfo-92@hotmail.com>
2022-05-08 17:45:32 -0300

    remoção dos arquivos da pasta

--- bd/Banco_producao.sql
+++ bd/Banco_producao.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Tempo de geração: 03-Maio-2022 às 13:34
+-- Tempo de geração: 08-Maio-2022 às 17:27
 -- Versão do servidor: 10.5.12-MariaDB-1
 -- versão do PHP: 7.4.26
 
@@ -37,18 +37,6 @@ CREATE TABLE `Comprador` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
--- Extraindo dados da tabela `Comprador`
---
-
-INSERT INTO `Comprador` (`ID`, `NOME`, `TELEFONE`, `EMAIL`, `STATUS`, `SITUACAO`) VALUES
-(1, 'MARCIO', '63992107848', 'Nenhum', 'Inativo', 'EM OPERACAO'),
-(2, 'LEONARDO', '63991057321', '-', 'Ativo', 'EM OPERAÇÃO'),
-(3, 'HELTON JHON', ' 63984668490 ', '-', 'Ativo', 'EM OPERAÇÃO'),
-(4, 'THAYS', '63999734300', '-', 'Ativo', 'EM OPERAÇÃO'),
-(5, 'PABLO', '63992457777', '-', 'Ativo', 'EM OPERAÇÃO'),
-(6, 'WITOR', ' 63984100146 ', '-', 'Ativo', 'EM OPERAÇÃO'),
-(7, 'ROMARIO', '63992704356', '-', 'Ativo', 'EM OPERAÇÃO'),
-(8, 'MÁRCIA', '63992639401', '-', 'Ativo', 'EM OPERAÇÃO');
 
 -- --------------------------------------------------------
 
@@ -71,9 +59,10 @@ CREATE TABLE `conta` (
 --
 
 INSERT INTO `conta` (`ID`, `NOME`, `EMAIL`, `SENHA`, `TIPO`, `STATUS`, `FOTO`) VALUES
-(1, 'Vilagrant Ataulfo', 'ataulfo192@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'Admin', 'Ativo', 'perfil/perfil.jpg'),
-(16, 'Ataulfo', 'ataulfo-92@gmail.com', 'c8837b23ff8aaa8a2dde915473ce0991', 'Admin', 'Ativo', 'imagens/utilizador.png'),
-(20, 'Jhony', 'jhony@gmail.com', 'e10adc3949ba59abbe56e057f20f883e', 'Usuario', 'Ativo', 'perfil/2024268.jpg');
+(1, 'Admin', 'admin@admin.com', 'e10adc3949ba59abbe56e057f20f883e', 'Admin', 'Ativo', 'perfil/detective-xxl.png'),
+(35, 'Eletric', 'eletric@gmail.com', 'c8837b23ff8aaa8a2dde915473ce0991', 'Usuario', 'Ativo', 'imagens/utilizador.png'),
+(36, 'Cosmo', 'cosmo@gmail.com', 'c8837b23ff8aaa8a2dde915473ce0991', 'Usuario', 'Ativo', 'perfil/5284734(1).jpg'),
+(37, 'Teste', 'teste@gmail.com', 'c8837b23ff8aaa8a2dde915473ce0991', 'Usuario', 'Ativo', 'imagens/utilizador.png');
 
 -- --------------------------------------------------------
 
@@ -98,71 +87,6 @@ CREATE TABLE `Fornecedor_lista` (
 -- Extraindo dados da tabela `Fornecedor_lista`
 --
 
-INSERT INTO `Fornecedor_lista` (`ID`, `NOME_FORNECEDOR`, `RECOLHIMENTO`, `TROCA_COND`, `TELA`, `COMPRADOR`, `QUEM_RECEBE`, `ESTADO`, `STATUS_GERAL`, `OPERACAO`) VALUES
-(1, 'Camil Sa', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Helton Jhon', 'CD', 1, 'Ativo', 'Loja / Avaria'),
-(2, 'Refrescante bandeirantes', 'Não', 'Nenhum', 'Movimentação', 'Helton Jhon', 'S/R', 2, 'Ativo', 'Quebra Operacional'),
-(3, 'Caramuru', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'CD', 1, 'Ativo', ''),
-(4, 'Pro varejo', 'Não', 'Nenhum', 'Movimentação', 'Leonardo', 'S/R', 3, 'Ativo', 'Quebra Bonificada'),
-(5, 'Edgard Co', 'Não', 'Nenhum', 'Movimentação', 'Pablo  ', 'S/R', 2, 'Ativo', ''),
-(6, 'Interbrilho', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Pablo', 'CD', 1, 'Ativo', ''),
-(7, 'Sanremo', 'Sim', 'Avaria', 'Movimentação', 'Pablo', 'CD', 3, 'Ativo', ''),
-(8, 'Temar', 'Não', 'Nenhum', 'Movimentação', 'Helton Jhon', 'S/R', 2, 'Ativo', ''),
-(9, 'Cicopal', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'CD', 1, 'Ativo', 'Loja / Avaria'),
-(10, 'Barila', 'Sim', 'Venc', 'Movimentação', 'Leonardo', 'F/L', 3, 'Ativo', ''),
-(11, 'Bem natural', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'CD', 1, 'Ativo', ''),
-(12, 'Apti', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'CD', 1, 'Ativo', ''),
-(13, 'Amafil', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'CD', 1, 'Ativo', ''),
-(14, 'Cepera', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'CD', 1, 'Ativo', ''),
-(15, 'Pandurata', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'CD', 1, 'Ativo', ''),
-(16, 'R de paula', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Helton Jhon', 'F/L', 1, 'Ativo', ''),
-(17, 'Dom jason', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'CD', 1, 'Ativo', 'Loja / Avaria'),
-(18, 'J macedo', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'CD', 1, 'Ativo', ''),
-(19, 'Nutri', 'Não', 'venc', 'Movimentação', 'Leonardo', 'S/R', 3, 'Ativo', ''),
-(20, 'Atacadao SA', 'Não', 'Nenhum', 'Movimentação', 'Leonardo', 'S/R', 2, 'Ativo', ''),
-(21, 'Enova foods', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'CD', 1, 'Ativo', ''),
-(22, 'Cafe paraiso', 'Não', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'F/L', 1, 'Ativo', ''),
-(23, 'Alvo dist', 'Sim', 'Venc', 'Transf / Locais', 'Leonardo', 'F/L', 1, 'Ativo', ''),
-(24, 'Fugini', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'CD', 1, 'Ativo', ''),
-(25, 'Fecularia lopes', 'Sim', 'Ava/Venc', 'Movimentação', 'Leonardo', 'S/R', 3, 'Ativo', ''),
-(26, 'Filho Distribuiçao', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'F/L', 1, 'Ativo', ''),
-(27, 'Goias verde', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'CD', 1, 'Ativo', ''),
-(28, 'Grafica ideal', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'CD', 1, 'Ativo', ''),
-(29, 'Imperatins', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'CD', 1, 'Ativo', ''),
-(30, 'Induspar', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'CD', 1, 'Ativo', ''),
-(31, 'Industria Alimenticia', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Leonardo', 'CD', 1, 'Ativo', ''),
-(33, 'Mr de Magalhaes', 'Não', 'Nenhum', 'Movimentação', 'Thays', 'S/R', 2, 'Ativo', ''),
-(34, 'Danone LTDA ', 'Não', 'Nenhum', 'Movimentação', 'Thays', 'S/R', 3, 'Ativo', 'Quebra Bonificada'),
-(35, 'Emporio Esmeralda', 'Não', 'Nenhum', 'Movimentação', 'Thays', 'S/R', 3, 'Ativo', ''),
-(36, 'BRF', 'Não', 'Nenhum', 'Movimentação', 'Thays', 'S/R', 2, 'Ativo', ''),
-(37, 'Trop frutas', 'Não', 'Nenhum', 'Movimentação', 'Thays', 'S/R', 2, 'Ativo', ''),
-(38, 'Watanabe', 'Não', 'Nenhum', 'Movimentação', 'Thays', 'S/R', 2, 'Ativo', ''),
-(39, 'Sao Salvador', 'Não', 'Nenhum', 'Movimentação', 'Thays', 'S/R', 2, 'Ativo', ''),
-(40, 'Santa Isabel Bonasa', 'Não', 'Nenhum', 'Movimentação', 'Thays', 'S/R', 2, 'Ativo', ''),
-(41, 'Laticinio Bela Vista', 'Não', 'Nenhum', 'Movimentação', 'Thays', 'S/R', 2, 'Ativo', ''),
-(42, 'Zuppani industrial', 'Não', 'Nenhum', 'Movimentação', 'Marcio', 'S/R', 2, 'Ativo', ''),
-(43, 'Quimica amparo', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Marcio', 'CD', 1, 'Ativo', ''),
-(44, 'Gsa gama', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Marcio', 'F/L', 1, 'Ativo', ''),
-(45, 'PONTELAND', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Marcio', 'CD', 1, 'Ativo', ''),
-(46, 'Maximo distribuidor', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Marcio', 'CD', 1, 'Ativo', ''),
-(47, 'Lebam comercio', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Marcio', 'CD', 1, 'Ativo', ''),
-(48, 'KIMBERLY CLARK', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Marcio', 'CD', 1, 'Ativo', ''),
-(49, 'Florence industrial', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Marcio', 'CD', 1, 'Ativo', ''),
-(50, 'GOIAS ALIMENTOS', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Marcio', 'CD', 1, 'Ativo', ''),
-(51, 'BOMBRIL SA', 'Não', 'Nenhum', 'Movimentação', 'Marcio', 'S/R', 2, 'Ativo', ''),
-(52, 'Etilux', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Pablo', 'CD', 1, 'Ativo', ''),
-(53, 'Doce Festa', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Pablo', 'CD', 1, 'Ativo', 'Loja / Avaria'),
-(54, 'Brasilux', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Pablo', 'CD', 1, 'Ativo', ''),
-(55, 'Plasutil ', 'Sim', 'avaria', 'Transf / Locais', 'Pablo', 'CD', 1, 'Ativo', ''),
-(56, 'Santa Maria', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Pablo', 'CD', 1, 'Ativo', ''),
-(57, 'Soprano', 'Não', 'Nenhum', 'Movimentação', 'Pablo', 'S/R', 2, 'Ativo', ''),
-(58, 'Stolf artigos', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Pablo', 'CD', 1, 'Ativo', ''),
-(59, 'Rosa de ouro', 'Não', 'Ava/Venc', 'Movimentação', 'Pablo', 'S/R', 3, 'Ativo', ''),
-(60, 'Ourolux', 'Não', 'Nenhum', 'Movimentação', 'Pablo', 'S/R', 2, 'Ativo', ''),
-(61, 'Junco', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Pablo', 'CD', 1, 'Ativo', ''),
-(62, 'Mor distribuidora', 'Sim', 'Ava/Venc', 'Transf / Locais', 'Pablo', 'CD', 1, 'Ativo', ''),
-(63, 'Cromos industria', 'Não', 'Nenhum', 'Movimentação', 'Pablo', 'S/R', 2, 'Ativo', ''),
-(64, 'Comercial ramiro', 'Sim', 'Ava', 'Transf / Locais', 'Pablo', 'CD', 1, 'Ativo', 'Loja / Avaria'),
-(65, 'Tramontina', 'Sim', 'avaria', 'Transf / Locais', 'Pablo', 'CD', 1, 'Ativo', '');
 
 --
 -- Índices para tabelas despejadas
@@ -201,7 +125,7 @@ ALTER TABLE `Comprador`
 -- AUTO_INCREMENT de tabela `conta`
 --
 ALTER TABLE `conta`
-  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=38;
 
 --
 -- AUTO_INCREMENT de tabela `Fornecedor_lista`
